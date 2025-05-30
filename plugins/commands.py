c3d35c6323b361be2ca70a15fbdc7dfa4f583c4d
Kichu <kichumon333@outlook.com>
2025-05-30 20:19:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,26 +56,27 @@ async def start(client, message):
         return
     
 if not await is_subscribed(message.from_user.id, client):
-        links = await create_invite_links(client)
-        btn = [[InlineKeyboardButton("🤖 Join Updates Channel", url=url)] for url in links.values()]
+    links = await create_invite_links(client)
+    btn = [[InlineKeyboardButton("🤖 Join Updates Channel", url=url)] for url in links.values()]
 
-        if len(message.command) == 2:
-            try:
-                kk, file_id = message.command[1].split("_", 1)
-                pre = 'checksubp' if kk == 'filep' else 'checksub'
-                btn.append([InlineKeyboardButton(" Try Again", callback_data=f"{pre}#{file_id}")])
-            except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton(" Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
-        
-        await client.send_message(
-            chat_id=message.from_user.id,
-            text="**Please Join My Updates Channel to use this Bot!**",
-            reply_markup=InlineKeyboardMarkup(btn),
-            parse_mode=enums.ParseMode.MARKDOWN
-        )
-        return
-    if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons = [[
+    if len(message.command) == 2:
+        try:
+            kk, file_id = message.command[1].split("_", 1)
+            pre = 'checksubp' if kk == 'filep' else 'checksub'
+            btn.append([InlineKeyboardButton(" Try Again", callback_data=f"{pre}#{file_id}")])
+        except (IndexError, ValueError):
+            btn.append([InlineKeyboardButton(" Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+    
+    await client.send_message(
+        chat_id=message.from_user.id,
+        text="**Please Join My Updates Channel to use this Bot!**",
+        reply_markup=InlineKeyboardMarkup(btn),
+        parse_mode=enums.ParseMode.MARKDOWN
+    )
+    return
+
+if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
+    buttons = [[
         InlineKeyboardButton('➕ Add Me To Your Group ➕', url=f"http://t.me/{temp.U_NAME}?startgroup=true")
     ], [
         InlineKeyboardButton('🔎 Search Here 🗂', switch_inline_query_current_chat='')
